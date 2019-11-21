ad0bb03e75b846a0664f26eb06fdf405a71efd32
IgorZyktin <Stark-shooter-2>
2019-11-21 15:18:36 +0300

    ограничено число строк при выводе в pandas

--- trivial_tools/pandas_tools/text.py
+++ trivial_tools/pandas_tools/text.py
@@ -8,7 +8,7 @@
 import pandas as pd
 
 
-def pandas_output(*args, max_rows: int = -1, max_columns: int = 100):
+def pandas_output(*args, max_rows: int = 100, max_columns: int = 100):
     """
     Распечатать с настройками контекста
     """

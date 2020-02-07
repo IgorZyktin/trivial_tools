6c539bae9971a56e14951d61390126bd8a5353d2
Igor Zyktin <nicord@yandex.ru>
2020-02-07 12:59:46 +0300

    исправлена ошибка импорта

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="trivial_tools",
-    version="8.10",
+    version="8.11",
     author="Igor Zyktin",
     author_email="nicord@yandex.ru",
     description="Trivial tools for various scripts",
